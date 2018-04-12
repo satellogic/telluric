c62b757f5fed7789977a4c87f1cf31a181c59166
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2018-04-12 08:10:47 +0200

    Latest additions to setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ setup(
     name='telluric',
     version=versioneer.get_version(),
     description='Interactive geospatial data manipulation in Python',
+    license="MIT",
+    long_description=open('README.md', encoding='utf-8').read(),
     author='Juan Luis Cano, Guy Doulberg, Slava Kerner, Lucio Torre, Ariel Zerahia',
     maintainer_email='juanlu@satellogic.com',
     url='https://github.com/satellogic/telluric/',
     download_url=(
-        'https://github.com/satellogic/telluric/tags/' +
+        'https://github.com/satellogic/telluric/tags/v' +
         versioneer.get_version()
     ),
     packages=find_packages(),
@@ -45,5 +47,22 @@ setup(
             'sphinx_rtd_theme'
         ],
     },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    zip_safe=False,
+    long_description_content_type='text/markdown',
     cmdclass=versioneer.get_cmdclass(),
 )
