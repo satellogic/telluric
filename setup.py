6eb42f60b0c92cced62a4c4013db9788cb38ae42
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2018-06-28 16:29:57 +0200

    Test in Python 3.7

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: GIS",
