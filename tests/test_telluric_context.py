322aa1b91aa682a5156baff5c71ad56d74c53961
Ariel Zerahia <arielz@satellogic.com>
2018-07-04 09:07:02 +0300

    updated context threads test

--- tests/test_telluric_context.py
+++ tests/test_telluric_context.py
@@ -28,31 +28,35 @@ def test_context_in_two_level():
 
 def test_different_context_on_different_threads():
     import threading
+    from time import sleep
 
-    class MyThread (threading.Thread):
-        def start(self):
-            with TelluricContext(a=1, b=2, c='stam', d={'a': 'a', 'b': 'b'}):
-                assert local_context.get('a') == 1
-                assert local_context.get('b') == 2
-                assert local_context.get('c') == 'stam'
-                assert local_context.get('d') == {'a': 'a', 'b': 'b'}
-                assert local_context.get('x') is None
-                assert local_context.get('y') is None
-            assert local_context._options is None
+    def thread_test_1():
+        with TelluricContext(a=1, b=2, c='stam', d={'a': 'a', 'b': 'b'}):
+            sleep(0.1)
+            assert local_context.get('a') == 1
+            assert local_context.get('b') == 2
+            assert local_context.get('c') == 'stam'
+            assert local_context.get('d') == {'a': 'a', 'b': 'b'}
+            assert local_context.get('x') is None
+            assert local_context.get('y') is None
+        assert local_context._options is None
 
-    class MyThread2 (threading.Thread):
-        def start(self):
-            with TelluricContext(a=4, b=5, x='something', y=13):
-                assert local_context.get('a') == 4
-                assert local_context.get('b') == 5
-                assert local_context.get('x') == 'something'
-                assert local_context.get('y') == 13
-                assert local_context.get('c') is None
-                assert local_context.get('d') is None
-            assert local_context._options is None
+    def thread_test_2():
+        with TelluricContext(a=4, b=5, x='something', y=13):
+            assert local_context.get('a') == 4
+            assert local_context.get('b') == 5
+            assert local_context.get('x') == 'something'
+            assert local_context.get('y') == 13
+            assert local_context.get('c') is None
+            assert local_context.get('d') is None
+            sleep(0.1)
+        assert local_context._options is None
 
+    t1 = threading.Thread(target=thread_test_1)
+    t2 = threading.Thread(target=thread_test_2)
+    t1.start()
+    t2.start()
     assert local_context._options is None
-    MyThread().start()
-    assert local_context._options is None
-    MyThread2().start()
+    t1.join()
+    t2.join()
     assert local_context._options is None
