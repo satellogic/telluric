f1e473e8c2de6c2a66974dab8973df90e5afb3d2
Guy D <guyd@satellogic.com>
2018-08-13 13:24:32 +0300

    code style

--- tests/test_georaster_tiling.py
+++ tests/test_georaster_tiling.py
@@ -93,7 +93,7 @@ class GeoRaster2TestGetTile(TestCase):
         path = os.path.join(cls.temp_dir.name, 'test_raster_wgs84.tif')
         # path = os.path.join("/tmp/tests/read_only_vgr_wgs84", 'test_raster_wgs84.tif')
         # if not os.path.isfile(path):
-            # cls.raster_for_test_wgs84().save(path)
+        # cls.raster_for_test_wgs84().save(path)
         url = "https://publicsatellogic.blob.core.windows.net/telluric-test-data/test_raster_wgs84.tif"
         cls.read_only_vgr_wgs84 = GeoRaster2.open(url)
 
