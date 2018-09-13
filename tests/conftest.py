92704ebc234341b12424252b59add553f20ee60d
Guy D <guyd@satellogic.com>
2018-09-13 11:30:44 +0300

    using reasterio CRS

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from affine import Affine
 import telluric as tl
 from telluric.constants import WEB_MERCATOR_CRS
 import pytest
+from rasterio.crs import CRS
 
 
 @pytest.fixture(scope="module")
@@ -10,7 +11,7 @@ def test_raster_with_no_url():
     raster = tl.GeoRaster2(np.random.uniform(0, 256, (3, 391, 370)),
                            affine=Affine(1.0000252884112817, 0.0, 2653750.345511198,
                                          0.0, -1.0000599330133702, 4594461.485763356),
-                           crs={'init': 'epsg:3857'})
+                           crs=CRS({'init': 'epsg:3857'}))
     return raster
 
 
