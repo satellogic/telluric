b6e83177f15b02b791165fd28867255aa5479856
Ariel Zerahia <arielz@satellogic.com>
2018-11-13 12:17:17 +0200

    adding resampling to crop to speed-up local tile serving

--- telluric/util/local_tile_server.py
+++ telluric/util/local_tile_server.py
@@ -5,21 +5,31 @@ import tornado.web
 from tornado import gen
 from threading import Thread, Lock
 from tornado.ioloop import IOLoop
+from rasterio.enums import Resampling
 from telluric.constants import WGS84_CRS
+from tornado.httpserver import HTTPServer
 
 
 class TileServerHandler(tornado.web.RequestHandler):
-    def initialize(self, rasters):
+    def initialize(self, rasters, resampling):
         self.rasters = rasters
+        self.resampling = resampling
 
     @tornado.web.asynchronous
     @gen.coroutine
     def get(self, raster_id, x, y, z):
+        png_tile = yield self._get_png_tile(int(raster_id), int(x), int(y), int(z))
+        if png_tile:
+            self.finish(png_tile)
+            self.set_header("Content-type", "image/png")
+        else:
+            self.send_error(404)
+
+    @gen.coroutine
+    def _get_png_tile(self, raster_id, x, y, z):
+        raster = self.rasters[raster_id]
         with rasterio.Env():
-            tile = self.rasters[int(raster_id)].get_tile(int(x), int(y), int(z))
-        self.write(tile.to_png())
-        self.set_header("Content-type", "image/png")
-        self.finish()
+            return raster.get_tile(x, y, z, resampling=self.resampling).to_png()
 
 
 class OKHandler(tornado.web.RequestHandler):
@@ -27,17 +37,17 @@ class OKHandler(tornado.web.RequestHandler):
         self.write("i'm alive")
 
 
-def make_app(rasters):
+def make_app(rasters, resampling):
     uri = r'/(\d+)/(\d+)/(\d+)/(\d+)\.png'
     return tornado.web.Application([
-        (uri, TileServerHandler, dict(rasters=rasters)),
+        (uri, TileServerHandler, dict(rasters=rasters, resampling=resampling)),
         (r'/ok', OKHandler),
     ])
 
 
-def _run_app(rasters, port=4000):
+def _run_app(rasters, resampling, port=4000):
     asyncio.set_event_loop(asyncio.new_event_loop())
-    app = make_app(rasters)
+    app = HTTPServer(make_app(rasters, resampling))
     app.listen(port, '0.0.0.0')
     IOLoop.current().start()
 
@@ -69,11 +79,11 @@ class TileServer:
         return "http://localhost:%s/%s/{x}/{y}/{z}.png" % (port, id(raster))
 
     @classmethod
-    def run_tileserver(cls, raster, port=4000):
+    def run_tileserver(cls, raster, resampling=Resampling.cubic, port=4000):
         cls.add_raster(raster)
         if cls.running_app is None:
             try:
-                cls.running_app = Thread(None, _run_app, args=(cls.rasters, port),
+                cls.running_app = Thread(None, _run_app, args=(cls.rasters, resampling, port),
                                          name='TileServer', daemon=True)
                 cls.running_app.start()
                 return cls.running_app
