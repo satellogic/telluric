869f4bab28caee238c49d04ae27e6925fdc1c2c9
drnextgis <rykovd@gmail.com>
2018-11-17 09:34:28 +0100

    Rasterio 1.0.10

--- telluric/__init__.py
+++ telluric/__init__.py
@@ -2,13 +2,7 @@ from telluric.georaster import GeoRaster2, MutableGeoRaster
 from telluric.vectors import GeoVector
 from telluric.features import GeoFeature
 from telluric.collections import FileCollection, FeatureCollection
-import rasterio
-from rasterio.crs import CRS
-
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
-
-with rasterio.Env():
-    CRS({'init': 'epsg:4326'}).is_geographic
