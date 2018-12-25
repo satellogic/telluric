08b1d260da2ee95880e1dcb3bae142612b217fb2
Ariel Zerahia <arielz@satellogic.com>
2018-12-25 09:52:37 +0200

    use non consuming method

--- telluric/georaster.py
+++ telluric/georaster.py
@@ -2026,7 +2026,7 @@ release, please use: .colorize('gray').to_png()", GeoRaster2Warning)
             return None
         elif len(assets) > 1:
             return GeoMultiRaster.from_assets(assets)
-        _, raster = assets.popitem()
+        raster = list(assets.values())[0]
         return GeoRaster2.open(raster["href"], band_names=raster["bands"])
 
 
