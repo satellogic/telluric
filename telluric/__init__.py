57611b5e1aea6195dc71cd8714175bfd41f10931
Ariel Zerahia <arielz@satellogic.com>
2018-12-27 14:45:29 +0200

    fix rebase issues

--- telluric/__init__.py
+++ telluric/__init__.py
@@ -1,6 +1,6 @@
 from telluric.georaster import GeoRaster2, MutableGeoRaster, GeoMultiRaster
 from telluric.vectors import GeoVector
-from telluric.features import GeoFeature, GeoFeatureWithRaster
+from telluric.features import GeoFeature
 from telluric.collections import FileCollection, FeatureCollection
 
 from ._version import get_versions
--- telluric/util/local_tile_server.py
+++ telluric/util/local_tile_server.py
@@ -41,8 +41,8 @@ class TileServerHandler(tornado.web.RequestHandler):
         if tile_vector.intersects(obj.footprint):
             if isinstance(obj.obj, tl.GeoRaster2):
                 tile = yield self._get_raster_png_tile(obj.obj, x, y, z)
-            if isinstance(obj.obj, tl.GeoFeatureWithRaster):
-                tile = yield self._get_raster_png_tile(obj.obj.raster, x, y, z)
+            if isinstance(obj.obj, tl.GeoFeature) and obj.obj.has_raster:
+                tile = yield self._get_raster_png_tile(obj.obj.raster(), x, y, z)
             elif isinstance(obj.obj, BaseCollection):
                 tile = yield self._get_collection_png_tile(obj.obj, x, y, z)
 
