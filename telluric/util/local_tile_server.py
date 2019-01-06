ab08b86924416c55f97f72198cb1c5fc4afaba5f
Ariel Zerahia <arielz@satellogic.com>
2019-01-06 16:53:28 +0200

    fix local tile server for collections

--- telluric/util/local_tile_server.py
+++ telluric/util/local_tile_server.py
@@ -44,7 +44,7 @@ class TileServerHandler(tornado.web.RequestHandler):
             if isinstance(obj.obj, tl.GeoFeature) and obj.obj.has_raster:
                 tile = yield self._get_raster_png_tile(obj.obj.raster(), x, y, z)
             elif isinstance(obj.obj, BaseCollection):
-                tile = yield self._get_collection_png_tile(obj.obj(), x, y, z)
+                tile = yield self._get_collection_png_tile(obj.obj, x, y, z)
 
             if tile:
                 self.set_header("Content-type", "image/png")
@@ -61,7 +61,7 @@ class TileServerHandler(tornado.web.RequestHandler):
 
     @gen.coroutine
     def _get_collection_png_tile(self, fc, x, y, z):
-        rasters = yield gen.multi([self._get_raster_png_tile(f.raster, x, y, z) for f in fc])
+        rasters = yield gen.multi([self._get_raster_png_tile(f.raster(), x, y, z) for f in fc])
         if len(rasters) < 1:
             return None
         tile = yield self._merge_rasters(rasters, z)
