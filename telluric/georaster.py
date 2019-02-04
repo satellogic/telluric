dfe5348b5cfe99617b0f9186dc9b6b4bee3934d7
Guy D <guyd@satellogic.com>
2019-02-04 12:47:57 +0200

    when doing a copy with image not loaded still don't load

--- telluric/georaster.py
+++ telluric/georaster.py
@@ -1218,7 +1218,7 @@ class GeoRaster2(WindowMethodsMixin, _Raster):
 
         # The image is a special case because we don't want to make a copy of a possibly big array
         # unless totally necessary
-        if 'image' not in init_args:
+        if 'image' not in init_args and not self.not_loaded():
             init_args['image'] = self.image
         if mutable:
             _cls = MutableGeoRaster
