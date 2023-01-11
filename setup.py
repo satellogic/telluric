ea02a477bdb9d611a0340ceaf0cbcf3df65db098
Denis Rykov <rykovd@gmail.com>
2023-01-11 23:03:55 +0100

    Drop Python 3.6 support

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     ),
     packages=find_packages(),
     include_package_data=True,
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=[
         'affine',
         'fiona>=1.8.4,<2.*',
@@ -59,7 +59,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
