e7d56e2ab766af19bc51f488f0708ba883336c00
Denis Rykov <rykovd@gmail.com>
2023-04-18 13:21:44 +0200

    Update dependency specification

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ setup(
     python_requires=">=3.7",
     install_requires=[
         'affine',
-        'fiona>=1.8.4,<2.*',
-        'shapely>=1.6.3,<2.*',
+        'fiona>=1.8.4,<=1.8.21',
+        'shapely>=1.6.3,<2.0.0',
         'rasterio>=1.2.0',
         'pillow',
         'mercantile>=0.10.0',
